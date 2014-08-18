c0b8b2ab09a77c04272d62321cfa0ca354e38b10
Jiaqi <lijiaqigreat@gmail.com>
2014-08-17 20:42:34 -0500

    add jade_json

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 var SRC='src';
 var DIST='dist';
+var jade_json_name='json';
 
 
 gulp.task('js', function () {
@@ -43,15 +44,25 @@ gulp.task('lib', function () {
     .pipe(gulp.dest(DIST+'/lib'))
     .pipe($.size({title: 'lib'}));
 });
+gulp.task('jade_json', function () {
+  delete $.cached.caches['jade'];
+  return gulp.src(SRC+'/jade_json/**/*.json')
+    .pipe($.cached('jade_json',['optimizeMemory']))
+    .pipe($.jsonminify())
+    .pipe($.header('- var '+jade_json_name+'='))
+    .pipe($.rename({extname:'.json.jade'}))
+    .pipe(gulp.dest(SRC+'/jade_json'))
+    .pipe($.size({title: 'jade_json'}));
+});
+gulp.task('jade_pre', function () {
+  delete $.cached.caches['jade'];
+  return gulp.src(SRC+'/jade/**/*.jade')
+    .pipe($.cached('jade_pre',['optimizeMemory']))
+    .pipe($.size({title: 'jade_json'}));
+});
 gulp.task('jade', function () {
   return gulp.src([SRC+'/**/*.jade'])
-    .pipe($.cached('jade',['optimizeMemory']))
-    .pipe($.jade())
-    .pipe(gulp.dest(DIST));
-});
-gulp.task('jade:all', function () {
-  delete $.cached.caches['jade'];
-  return gulp.src([SRC+'/**/*.jade'])
+    .pipe($.ignore.exclude(SRC+'/jade'))
     .pipe($.cached('jade',['optimizeMemory']))
     .pipe($.jade())
     .pipe(gulp.dest(DIST));
@@ -59,11 +70,12 @@ gulp.task('jade:all', function () {
 
 gulp.task('clean', function(cb) {
   del([DIST],cb);
-  $.cached.caches={}
+  $.cached.caches={};
 });
 
 gulp.task('build',function(){
-  runSequence(['js','css','fonts','images','lib','jade'])});
+  runSequence(['js','css','fonts','images','lib','jade_json','jade_pre','jade'])
+});
 
 
 // Watch Files For Changes & Reload
@@ -80,6 +92,11 @@ gulp.task('serve', ['build'], function () {
   gulp.watch([SRC+'/fonts'], ['fonts',reload]);
   gulp.watch([SRC+'/images'], ['images',reload]);
   gulp.watch([SRC+'/lib'], ['lib',reload]);
-  gulp.watch([SRC+'/jade'], ['jade:all',reload]);
+  gulp.watch([SRC+'/jade_json/**/*.json'], ['jade_json','jade',reload]);
+  gulp.watch([SRC+'/jade'], ['jade_pre','jade',reload]);
   gulp.watch([SRC+'/**/*.jade'], ['jade',reload]);
+});
+gulp.task('test', function () {
+  delete $.cached.caches['jade_json'];
+  gulp.watch([SRC+'/jade_json/**/*.json'], ['jade_json','jade',reload]);
 });
